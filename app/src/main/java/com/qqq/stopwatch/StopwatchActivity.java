cf4e5469f866b5efd23ba946bfb21c76418a2654
grabblesaur <bogdanov.bayar@gmail.com>
2016-05-26 22:27:46 +0900

    asdsad

--- app/src/main/java/com/qqq/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/qqq/stopwatch/StopwatchActivity.java
@@ -14,6 +14,7 @@ public class StopwatchActivity extends AppCompatActivity {
     private Button mResetButton;
     private boolean isRunning;
     private int mSeconds;
+    private boolean wasRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +25,12 @@ public class StopwatchActivity extends AppCompatActivity {
         mStartButton = (Button) findViewById(R.id.start_button);
         mStopButton = (Button) findViewById(R.id.stop_button);
         mResetButton = (Button) findViewById(R.id.reset_button);
+
+        if (savedInstanceState != null) {
+            isRunning = savedInstanceState.getBoolean(getString(R.string.is_running_key));
+            mSeconds = savedInstanceState.getInt(getString(R.string.seconds_key));
+            wasRunning = savedInstanceState.getBoolean(getString(R.string.was_running_key));
+        }
 
         mStartButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,7 +57,59 @@ public class StopwatchActivity extends AppCompatActivity {
         runTimer();
     }
 
-    private void runTimer() {
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        if (wasRunning) {
+//            isRunning = true;
+//        }
+//    }
 
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        wasRunning = isRunning;
+//        isRunning = false;
+//    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        wasRunning = isRunning;
+        isRunning = false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (wasRunning) {
+            isRunning = true;
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean(getString(R.string.is_running_key), isRunning);
+        outState.putInt(getString(R.string.seconds_key), mSeconds);
+        outState.putBoolean(getString(R.string.was_running_key), wasRunning);
+    }
+
+    private void runTimer() {
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = mSeconds / 3600;
+                int minutes = (mSeconds % 3600) / 60;
+                int seconds = mSeconds % 60;
+                String time = String.format("%d:%02d:%02d",
+                        hours, minutes, seconds);
+                mTimer.setText(time);
+                if (isRunning) {
+                    mSeconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 }
